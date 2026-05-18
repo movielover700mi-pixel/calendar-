0a503598905b3d6072a5b6474718000af115f2ad
movielover700mi-pixel <movielover700mi@gmail.com>
2026-05-19 00:06:38 +0600

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# সরাসরি Environment Variable থেকে নিবে
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-# নোটিফিকেশন সেটিংস
-NOTIFICATION_HOUR = 8
-NOTIFICATION_MINUTE = 0
-TIMEZONE = "Asia/Dhaka"

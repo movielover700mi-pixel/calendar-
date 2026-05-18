50b298dd7586908047f480950765e7e700722a4d
movielover700mi-pixel <movielover700mi@gmail.com>
2026-05-18 21:39:59 +0600

    config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+# সরাসরি Environment Variable থেকে নিবে
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# নোটিফিকেশন সেটিংস
+NOTIFICATION_HOUR = 8
+NOTIFICATION_MINUTE = 0
+TIMEZONE = "Asia/Dhaka"
